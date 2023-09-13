d7f72f804b5c313cb1fc2fc2e6165dcc581afec0
Madu-Montes <monteseduarda56@gmail.com>
2023-09-13 14:35:45 -0300

    pasta atualizada

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,8 @@
-var ul = document.querySelector('nav ul');
-var menuBtn = document.querySelector('.menu-btn i');
-
-function menuShow() {
-    if (ul.classList.contains('open')) {
-        ul.classList.remove('open');
-    }else{
-        ul.classList.add('open');
-    }
+function show(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
 }
+
+windowsr = ScrollReveal({reset: true });
+
+sr.reveal('.box-sobre img , .cursos-formacao, .habilidades, .box', {duration: 1500});
